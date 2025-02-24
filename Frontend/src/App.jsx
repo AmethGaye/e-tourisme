553ee3745276c0900d35079e7460d2caf832acef
ahmada <amethgaye1401@gmail.com>
2025-02-24 12:00:58 +0000

    initialisation de la partie frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-gray-700 m-8 p-6 bg-zinc-100 text-center rounded-lg">
+        Bienvenu dans Sen Edu{`'`}Trip
+      </h1>
+    </>
+  )
+}
+
+export default App
